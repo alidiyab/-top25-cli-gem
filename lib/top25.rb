76c53384e399f66d1c636ca2b4a5d2dd6b47e3af
Ali Diyab <alidiyab1997@gmail.com>
2017-01-26 10:17:30 +0000

    new repo

--- lib/top25.rb
+++ lib/top25.rb
@@ -1,5 +1,11 @@
-require "top25/version"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+require 'colorize'
+require_relative "./top25/version"
+require_relative "./top25/cli"
+require_relative "./top25/place"
+require_relative "./top25/scraper"
 
 module Top25
-  # Your code goes here...
 end
